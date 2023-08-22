83e73fb86d3888a3980b04a03e913d90cc17d134
Laurie Atkinson <Laurie@TruDate.me>
2023-08-22 14:38:31 -0600

    add android manifest settings and set permissions

--- src/screens/ChannelScreen.tsx
+++ src/screens/ChannelScreen.tsx
@@ -1,31 +1,39 @@
 import {Channel, MessageInput, MessageList} from 'stream-chat-react-native';
 import {useAppContext} from '../context/AppContext';
-import {Text} from 'react-native';
+import {PermissionsAndroid, Platform, Text} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {NavigationStackParamsList} from '../types';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 type ChannelScreenProps = StackScreenProps<
   NavigationStackParamsList,
   'ChannelScreen'
 >;
 
-export const ChannelScreen: React.FC<ChannelScreenProps> = props => {
-  const {channel, setThread} = useAppContext();
-  const {navigation} = props;
+export const ChannelScreen: React.FC<ChannelScreenProps> = () => {
+  const {channel} = useAppContext();
+  const [permissionRequested, setPermissionRequested] = React.useState(false);
 
-  if (!channel) return <Text>Loading</Text>;
+  useEffect(() => {
+    const requestCameraAndFilePermission = async () => {
+      await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      ]);
+      setPermissionRequested(true);
+    };
+    if (Platform.OS === 'android' && !permissionRequested) {
+      requestCameraAndFilePermission();
+    }
+  }, [permissionRequested]);
+
+  if (!channel) {
+    return <Text>Loading</Text>;
+  }
 
   return (
-    <Channel channel={channel}>
-      <MessageList
-        onThreadSelect={message => {
-          if (channel?.id) {
-            setThread(message);
-            navigation.navigate('ThreadScreen');
-          }
-        }}
-      />
+    <Channel channel={channel} hasFilePicker={false} hasImagePicker={true}>
+      <MessageList />
       <MessageInput />
     </Channel>
   );
